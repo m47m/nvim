7dca04967aaca2f2aa3fbe45b049c1047e264978
m47m <xm130812@163.com>
2023-06-08 17:19:16 +0800

    something wrong with bufferline

---
+++ lua/failed/prettier.lua
@@ -0,0 +1,23 @@
+-- local M = {
+-- 	'MunifTanjim/prettier.nvim',
+-- }
+-- function M.config()
+-- 	local status ,prettier = pcall(require,'prettier')
+-- 	if (not status) then return end
+--
+-- 	prettier.setup{
+-- 		bin = 'prettierd',
+-- 		filetypes = {
+-- 			'css',
+-- 			'javascript',
+-- 			'javascriptreact',
+-- 			'typescript',
+-- 			'typescriptreact',
+-- 			'json',
+-- 			'scss',
+-- 			'less'
+-- 		}
+-- 	}
+-- end
+
+-- return M 
